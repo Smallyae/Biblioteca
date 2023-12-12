cd812244b1f3c558660b71d3f90cedff55e6eb70
Yae <106218352+Smallyae@users.noreply.github.com>
2023-12-12 10:50:06 -0500

    Diseño menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var boton = document.querySelector("#btn");
 var menu = document.querySelector("header .hamburguesa");
+var topLine = document.querySelector(".line.top");
+var middleLine = document.querySelector(".line.middle");
+var bottomLine = document.querySelector(".line.bottom");
 
 boton.addEventListener("click", function () {
   menu.classList.toggle("mostrar");
+  topLine.classList.toggle("top-clicked");
+  middleLine.classList.toggle("middle-clicked");
+  bottomLine.classList.toggle("bottom-clicked");
 });
